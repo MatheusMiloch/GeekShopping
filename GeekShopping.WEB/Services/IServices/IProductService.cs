fffd17669a630d9542269816b5767f1d38310c2c
Matheus Miloch <matheusmilochrosado@gmail.com>
2022-02-14 17:05:27 -0300

    Projetos Web, JWT e API

---
+++ GeekShopping.WEB/Services/IServices/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using GeekShopping.WEB.Models;
+
+namespace GeekShopping.WEB.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAllProducts(string token);
+
+        Task<ProductModel> FindProductById(long id, string token);
+
+        Task<ProductModel> CreateProduct(ProductModel model, string token);
+
+        Task<ProductModel> UpdateProduct(ProductModel model, string token);
+
+        Task<bool> DeleteProductById(long id, string token);
+    }
+}
